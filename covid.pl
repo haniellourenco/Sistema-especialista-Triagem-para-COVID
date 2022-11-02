b88b1f231efea3e8c6928f6e723535038ecb62b4
Haniel Lohn <hanielohn2016@gmail.com>
2022-11-01 23:41:09 -0300

    ?????????????

--- covid.pl
+++ covid.pl
@@ -1,41 +1,78 @@
-:- dynamic triagem/2.
+:- dynamic paciente/3.
 
-covid :- carrega('pacientes.bd'),
-    format('~n*** Triagem ***~n~n'),
+covid :- carrega('paciente.bd'),
+    format('~n*** Paciente ***~n~n'),
     repeat,
-    pergunta(Paciente),
-    pergunta(Paciente, Temperatura),
-    responde(Paciente),
+    nome(Nome),
+    temperatura(Nome),
+    freq_cardiaca(Nome),
+    freq_respiratoria(Nome),
+    pa_sistolica(Nome),
+    saturacao(Nome),
+    dispneia(Nome),
+    idade(Nome),
+    comorbidades(Nome),
+    responde(Nome),
+    salva(paciente,'paciente.bd'),
     continua(Resposta),
     Resposta = n,
-    !,
-    salva(capital,'pacientes.bd').
+    !.
 
 carrega(Arquivo) :-
     exists_file(Arquivo),
     consult(Arquivo);
     true.
 
-pergunta(Paciente) :-
+nome(Nome) :-
     format('~nQual o nome do paciente? '),
-    gets(Paciente).
+    gets(Nome).
 
-pergunta(Temperatura, Paciente) :-
-    format('~nQual a temperatura do paciente ~w? ',[Paciente]),
+temperatura(Temperatura) :-
+    format('~nQual a temperatura de ~w? ', [Nome]),
     gets(Temperatura),
-    asserta(triagem(Paciente,Temperatura)).
+    asserta(paciente(Nome, temperatura, Temperatura)).
+
+freq_cardiaca(FreqCard):-
+    format('~nQual a frequência cardíaca de ~w? ', [Nome]),
+    gets(FreqCard),
+    asserta(paciente(Nome, freq_cardiaca, FreqCard)).
 
 
-responde(Paciente) :-
-    triagem(Paciente),
-    !,
-    format('A paciente de ~w tem  de temperatura ', [Paciente]).
+freq_respiratoria(FreqResp):-
+    format('~nQual a frequência respiratória de ~w? ', [Nome]),
+    gets(FreqResp),
+    asserta(paciente(Nome, freq_respiratoria, FreqResp)).
 
-responde(Idade) :-
-    format('Qual � a idade de ~w? ', [Paciente]),
+pa_sistolica(PaSis):-
+    format('~nQual a PA Sistólica de ~w? ', [Nome]),
+    gets(PaSis),
+    asserta(paciente(Nome, pa_sistolica, PaSis)).
+
+saturacao(Saturacao):-
+    format('~nQual a saturação de ~w? ', [Nome]),
+    gets(Saturacao),
+    asserta(paciente(Nome, saturacao, Saturacao)).
+
+dispneia(TemDispneia):-
+    format('~n~w tem dispnéia? ', [Nome]),
+    gets(TemDispneia),
+    asserta(paciente(Nome, dispneia, TemDispneia)).
+
+idade(Idade):-
+    format('~nQual a idade de ~w? ', [Nome]),
     gets(Idade),
-    asserta(triagem(Paciente, Idade)).
+    asserta(paciente(Nome, idade, Idade)).
 
+comorbidades(NumComorb):-
+    format('~n~w possui quantas comorbidades? ', [Nome]),
+    gets(NumComorb),
+    asserta(paciente(Nome, comorbidades, NumComorb)).
+
+
+responde(Nome):-
+    gravidade(Nome, Char),
+    !,
+    format('A gravidade de ~w é ~w',[Nome,Char]).
 continua(Resposta) :-
     format('~nContinua? [s/n] '),
     get_char(Resposta),
@@ -45,3 +82,28 @@ gets(String) :-
     read_line_to_codes(user_input,Char),
     name(String,Char).
 
+
+
+salva(Paciente,Arquivo):-
+    tell(Arquivo),
+    listing(Paciente),
+    told.
+
+gravidade(Nome, grave):-
+    paciente(Nome, freq_respiratoria,FreqResp), FreqResp > 30,
+    paciente(Nome, pa_sistolica,PaSis),PaSis < 90,
+    paciente(Nome, saturacao,Saturacao), Saturacao < 95,
+    paciente(Nome, dispneia,TemDispneia), TemDispneia = "sim".
+
+gravidade(Nome, medio):-
+    paciente(Nome, temperatura,Temperatura), Temperatura > 39,
+    paciente(Nome, pa_sistolica,PaSis),PaSis >=90,PaSis =< 100,
+    paciente(Nome, idade,Idade), Idade > 80,
+    paciente(Nome, comorbidades,NumComorb), NumComorb > 1.
+
+gravidade(Nome, leve):-
+    paciente(Nome, temperatura,Temperatura), Temperatura < 35, (Temperatura >= 37, Temperatura =< 39),
+    paciente(Nome, freq_cardiaca,FreqCard),FreqCard > 100,
+    paciente(Nome, freq_respiratoria,FreqResp),FreqResp >= 19, FreqResp =< 30,
+    paciente(Nome, idade,Idade), Idade >= 60, Idade =< 79,
+    paciente(Nome, comorbidades,NumComorb), NumComorb = 1.
